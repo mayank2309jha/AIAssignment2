f9775b2738fc0d8a074231e872972e586a5631d5
Mayank Jha <78499893+mayank2309jha@users.noreply.github.com>
2023-10-31 21:27:32 +0530

    Update 2020521.py

--- 2020521.py
+++ 2020521.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 import queue
 from collections import deque
 from haversine import haversine as haversine_formula
@@ -10,7 +11,55 @@ data = pd.read_csv("Road_Distance.csv")
 data.replace("-", 0, inplace=True)
 
 # the dictionary that contains the coordinates for the heuristic
-city_coordinates = pd.read_csv("coordinates.csv").to_dict(orient='dict')
+city_coordinates = {
+    'Cochin': (9.9312, 76.2673),
+    'Jabalpur': (23.1815, 79.9864),
+    'Vishakapatnam': (17.6868, 83.2185),
+    'Bombay': (19.0760, 72.8777),
+    'Hyderabad': (17.3850, 78.4867),
+    'Lucknow': (26.8467, 80.9462),
+    'Imphal': (24.8170, 93.9368),
+    'Amritsar': (31.6340, 74.8723),
+    'Bangalore': (12.9716, 77.5946),
+    'Indore': (22.7196, 75.8577),
+    'Madras': (13.0827, 80.2707),
+    'Pune': (18.5204, 73.8567),
+    'Shimla': (31.1048, 77.1734),
+    'Asansol': (23.6760, 86.9784),
+    'Shillong': (25.5760, 91.8820),
+    'Panjim': (15.4909, 73.8278),
+    'Gwalior': (26.2183, 78.1828),
+    'Kolhapur': (16.7050, 74.2433),
+    'Varanasi': (25.3176, 82.9739),
+    'Baroda': (22.3072, 73.1812),
+    'Calcutta': (22.5726, 88.3639),
+    'Allahabad': (25.4358, 81.8463),
+    'Meerut': (28.6139, 77.2828),
+    'Nasik': (19.9975, 73.7898),
+    'Jaipur': (26.9124, 75.7873),
+    'Surat': (21.1702, 72.8311),
+    'Jamshedpur': (22.8046, 86.2029),
+    'Chandigarh': (30.7333, 76.7794),
+    'Calicut': (11.2588, 75.7804),
+    'Madurai': (9.9252, 78.1198),
+    'Hubli': (15.3647, 75.1240),
+    'Vijayawada': (16.5062, 80.6480),
+    'Ahmedabad': (23.0225, 72.5714),
+    'Ranchi': (23.3441, 85.3096),
+    'Bhopal': (23.2599, 77.4126),
+    'Bhubaneshwar': (20.2961, 85.8245),
+    'Delhi': (28.6139, 77.2090),
+    'Nagpur': (21.1458, 79.0882),
+    'Agra': (27.1767, 78.0081),
+    'Coimbatore': (11.0168, 76.9558),
+    'Kanpur': (26.4499, 80.3319),
+    'Trivandrum': (8.5241, 76.9366),
+    'Pondicherry': (11.9139, 79.8145),
+    'Ludhiana': (30.9010, 75.8573),
+    'Agartala': (23.8315, 91.2868),
+    'Patna': (25.5941, 85.1376),
+    'Jullundur': (31.3260, 75.5762),
+}
 
 # the set of all the cities
 cities = set()
@@ -114,22 +163,29 @@ def get_Path(start, goal):
         curr = parent[curr]
     stack.append(start)
     while len(stack) > 0:
-        print(stack.pop())
+        city = stack.pop()
+        if len(stack) == 0:
+            print(city ,end=" : ")
+        else:
+            print(f"{city} -> ",end ="")
+    print(f"Distance to the goal = {distances[goal]}")
 
 # the function to get the haversine distance between two cities
-def haversine(node, neighbour):
+def haversine(node, goal):
     nc = city_coordinates[node]
-    nec = city_coordinates[neighbour]
-    ans = haversine_formula(nc, nec)
+    nec = city_coordinates[goal]
+    given_distance_node_goal = float('inf') if goal not in graph[node] else graph[node][goal]
+    given_distance_goal_node = float('inf') if goal not in graph[goal] else graph[goal][node]
+    given_distance = min(given_distance_node_goal, given_distance_goal_node)
+    ans = min(haversine_formula(nc, nec),given_distance)
     return ans
 
 # the function to get the euclidean distance between two cities
-def euclidean_distance(node, neighbour):
-    # Calculate Euclidean distance
-    nc = tuple(city_coordinates[node])
-    nec = tuple(city_coordinates[neighbour])
-    ans = euclidean(nc, nec)
-    return ans
+def h_random(start, goal):
+    base_heuristic = haversine(start, goal)
+    max_random_offset = base_heuristic*5
+    random_offset = random.uniform(0, max_random_offset)
+    return base_heuristic + random_offset
 
 g = {}
 def a_star_search(start,goal,h):
@@ -156,27 +212,46 @@ def a_star_search(start,goal,h):
     heuristic = {}
     for city in cities:
         if h == "inadmissible":
-            heuristic[city] = euclidean_distance(city,goal)
+            heuristic[city] = h_random(city,goal)
         else:
             heuristic[city] = haversine(city,goal)
-        
+
     # minimum distance to the goal node
     minimum_to_goal = float('inf')
-    
+    count = 1
     while frontier.empty() == False:
         (f, city) = frontier.get()
         if f <= minimum_to_goal:
-            print(f'Current city = {city} at dist = {f}. Min distance to goal = {minimum_to_goal}')
+            # print(f'{count} Expand {city}. Min distance to goal = {minimum_to_goal}')
+            count = count + 1
             # we expand this node
             for neighbour,distance in graph[city].items():
                 # if we can reach this neighbour in distance smaller than g[neighbour] then we update it
                 # print(f"{neighbour} {distance}")
                 if g[city] + distance < g[neighbour]:
                     g[neighbour] = distance + g[city]
+                    parent[neighbour] = city
                     if neighbour == goal:
                         minimum_to_goal = min(minimum_to_goal,g[neighbour])
                     frontier.put((g[neighbour]+heuristic[neighbour],neighbour))
                 reached[city] = f
+
+    # to get the path
+    print()
+    print("Here comes the path")
+    stack = deque()
+    curr = goal
+    while curr != start:
+        stack.append(curr)
+        curr = parent[curr]
+    stack.append(start)
+    while len(stack) > 0:
+        city = stack.pop()
+        if len(stack) == 0:
+            print(city ,end=" : ")
+        else:
+            print(f"{city} -> ",end ="")
+    print(f"Distance to the goal = {g[goal]}")
 
 if __name__ == "__main__":
     flag = True
@@ -202,3 +277,28 @@ if __name__ == "__main__":
             flag = False
         else:
             print("You seem to have made a mistake.")
+    print("------------------------------------------------------------------------------------")
+    '''
+    for city1 in cities:
+        for city2 in cities:
+            if city1 != city2:
+                print("First dijkstra:-")
+                dijkstra(city1)
+                get_Path(city1,city2)
+
+                print("A* with admissible heuristic:-")
+                a_star_search(city1,city2,"admissible")
+
+                print("A* with non admissible heuristic:-")
+                a_star_search(city1,city2,"inadmissible")
+    '''
+
+
+'''
+Surat to Delhi gives the wrong answer with in admissible heuristic
+Coimbatore to Delhi as well
+
+To find out cases in which the nodes expanded were less:-
+Delhi to Imphal
+Delhi to Kanpur
+'''
